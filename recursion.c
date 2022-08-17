a30e8d6607fdffcbf06f6c9e611f6bb964aec8e1
PaulFierman <110355809+PaulFierman@users.noreply.github.com>
2022-08-17 02:28:57 -0400

    Create recursion.c

---
+++ recursion.c
@@ -0,0 +1,19 @@
+/* Paul Fierman */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "get.h"
+void recurDerivative(int aVal, int bVal,int* curr){
+        int* aVal_array = (int*)malloc(bVal* sizeof(int));
+        if(bVal > 0) {
+                aVal = getA(aVal,bVal);
+                bVal = getB(bVal);
+                aVal_array[*curr] = aVal;
+                printf("deravitive %d = %dx^%d\n", *curr+1, aVal_array[*curr],bVal);
+                (*curr)+=1;
+                recurDerivative(aVal, bVal, curr);
+        } else {
+                printf("deravitive %d = %d\n", *curr+1, 0);
+                free(aVal_array);
+        }
+}
